ffdf4552289a61cc0a2e448c8604d90285052b1c
tarun-jaikishan <tarunjaikishanpk@gmail.com>
2025-05-13 17:32:55 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+
+const fetchPosts = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  if (!response.ok) throw new Error("Error fetching posts");
+
+  return response.json();
+};
+
 export default function App() {
-  return <div>App</div>;
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <div>
+      {data.map((post: any, i: number) => (
+        <p key={i}>{post.title}</p>
+      ))}
+    </div>
+  );
 }
